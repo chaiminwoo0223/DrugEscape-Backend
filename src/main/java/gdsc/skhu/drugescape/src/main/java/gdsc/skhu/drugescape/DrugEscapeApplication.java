4be0c9d82df4fe3a3f885a12810976adb425077d
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2024-01-07 23:36:30 +0900

    로그인 성능 개선 2.4

---
+++ src/main/java/gdsc/skhu/drugescape/src/main/java/gdsc/skhu/drugescape/DrugEscapeApplication.java
@@ -0,0 +1,13 @@
+package gdsc.skhu.drugescape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugEscapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugEscapeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/gdsc/skhu/drugescape/src/main/java/gdsc/skhu/drugescape/domain/model/Role.java
@@ -0,0 +1,10 @@
+package gdsc.skhu.drugescape.domain.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum Role {
+    USER, ADMIN
+}
