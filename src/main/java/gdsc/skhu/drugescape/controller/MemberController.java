55cfe182024a55ccd131a9ccf8df07958bacf893
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2024-01-14 00:21:08 +0900

    로그인/회원가입 기능 POST로 변경 + 메인페이지 추가

--- src/main/java/gdsc/skhu/drugescape/controller/MemberController.java
+++ src/main/java/gdsc/skhu/drugescape/controller/MemberController.java
@@ -24,24 +24,24 @@ import org.springframework.web.server.ResponseStatusException;
 public class MemberController {
     private final MemberService memberService;
 
-    @Operation(summary = "Google OAuth2 로그인/회원가입", description = "Google OAuth2를 통한 로그인 및 회원가입을 처리합니다.")
+    @Operation(summary = "로그인/회원가입", description = "Google OAuth2를 통한 로그인 및 회원가입을 처리합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "로그인/회원가입 성공", content = @Content),
             @ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class))),
             @ApiResponse(responseCode = "401", description = "인증 실패", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class))),
             @ApiResponse(responseCode = "500", description = "서버 내부 오류", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class)))
     })
-    @GetMapping("/LoginSignup") // 요청 방식을 구체화 Get or Post or Request
-    public TokenDTO googleLoginSignup(@RequestParam(name = "code") String code) {
+    @PostMapping("/LoginSignup")
+    public ResponseEntity<TokenDTO> googleLoginSignup(@RequestBody TokenDTO tokenDTO) {
         try {
-            String googleAccessToken = memberService.getGoogleAccessToken(code);
-            return memberService.loginOrSignUp(googleAccessToken);
+            TokenDTO newToken = memberService.loginOrSignUp(tokenDTO.getRefreshToken());
+            return ResponseEntity.ok(newToken);
         } catch (RuntimeException e) {
-            log.error("Runtime Exception: ", e);
+            log.error("런타임 예외 발생: ", e);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
         } catch (Exception e) {
-            log.error("Unhandled Exception: ", e);
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred", e);
+            log.error("처리되지 않은 예외 발생: ", e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "예기치 않은 오류가 발생했습니다", e);
         }
     }
 
@@ -65,5 +65,15 @@ public class MemberController {
     public ResponseEntity<TokenDTO> refresh(@RequestBody TokenDTO tokenDTO) {
         TokenDTO newToken = memberService.refresh(tokenDTO.getRefreshToken());
         return ResponseEntity.ok(newToken);
+    }
+
+    @Operation(summary = "메인 페이지로 이동", description = "메인 페이지로 이동합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "메인 페이지로 이동 성공"),
+            @ApiResponse(responseCode = "401", description = "인증 실패", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class)))
+    })
+    @GetMapping("/main") // 여기서 유저와 관리자를 나눌 필요가 있다.(비밀번호 설정)
+    public String Main() {
+        return "메인 페이지로 넘어갑니다.";
     }
 }
